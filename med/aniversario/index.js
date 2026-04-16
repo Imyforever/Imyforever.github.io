0625fce702ee01a45ebb8644b71e47f4cedd48d3
Imyforever <dzul093@gmail.com>
2026-04-16 11:39:28 -0600

    aniversario

---
+++ med/aniversario/index.js
@@ -0,0 +1,23 @@
+const targetDate = new Date(2026, 3, 20, 18, 0, 0).getTime();
+
+const countdownInterval = setInterval(() => {
+    const now = new Date().getTime();
+    const distance = targetDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerText = days.toString().padStart(2, '0');
+    document.getElementById("hours").innerText = hours.toString().padStart(2, '0');
+    document.getElementById("minutes").innerText = minutes.toString().padStart(2, '0');
+    document.getElementById("seconds").innerText = seconds.toString().padStart(2, '0');
+
+    if (distance < 0) {
+        clearInterval(countdownInterval);
+        document.getElementById("countdown").classList.add("hidden");
+        document.getElementById("title").classList.add("hidden");
+        document.getElementById("unlocked-message").classList.remove("hidden");
+    }
+}, 1000);
